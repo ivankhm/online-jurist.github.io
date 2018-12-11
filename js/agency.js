ed60cadeeab0d64b6986ce04d37a2ecbf5056133
Ivan Khmelevskiy <ivan.khm@gmail.com>
2018-12-11 16:15:39 +0300

    Купить фикс 2

--- js/agency.js
+++ js/agency.js
@@ -1,6 +1,9 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  $('#checkout').on('shown.bs.modal', function () {
+    $('#checkout').trigger('focus')
+  });
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
